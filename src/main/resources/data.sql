6eec35fa1c2565a3c0e216c189958c37c580785d
Rajat Rajan <tug05734@temple.edu>
2021-04-15 17:13:22 -0400

    Edited Category

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,6 +4,10 @@ ON CONFLICT (role_id) DO UPDATE
 SET role = excluded.role;
 
 INSERT INTO category(id, name)
-VALUES (1, 'Album');
+VALUES (1, 'Album')
+ON CONFLICT (id) DO UPDATE
+SET name = excluded.name;
 INSERT INTO category(id, name)
-VALUES (2, 'Song');
+VALUES (2, 'Song')
+ON CONFLICT (id) DO UPDATE
+SET name = excluded.name;
