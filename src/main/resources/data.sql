e18408f75ba08ad1db0e53787a0dfc4a93ef81d9
Rajat Rajan <tug05734@temple.edu>
2021-04-15 17:29:30 -0400

    Edited Genre

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -8,5 +8,7 @@ VALUES (1, 'Album'), (2, 'Song'), (3, 'Instrument'), (4, 'Record'), (5, 'CD')
 ON CONFLICT (id) DO UPDATE
 SET name = excluded.name;
 
-INSERT INTO genre(name)
-VALUES ('Strings');
+INSERT INTO genre(id, name)
+VALUES (1, 'Strings'), (2, 'Woodwind'), (3, 'Brass'), (4, 'Percussion'), (5, 'Keyboard'), (6, 'Rock'), (7, 'Hip Hop'), (8, 'Jazz'), (9, 'Pop'), (10, 'Blues'), (11, 'Country')
+ON CONFLICT (id) DO UPDATE
+SET name = excluded.name;
